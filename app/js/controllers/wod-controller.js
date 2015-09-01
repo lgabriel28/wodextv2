13a29865c43246924f7d23a6a1f5ab37e217cc91
Luis Santiago <luis.santiagoayala@gmail.com>
2015-08-31 19:33:54 -0700

    Add comments for wod controller.

--- app/js/controllers/wod-controller.js
+++ app/js/controllers/wod-controller.js
@@ -1,25 +1,28 @@
 angular.module('myApp')
 .controller('WodController', function($scope, $routeParams, $sce, Wod, Video) {
 
+// Extract workout configuration parameters passed in url.
 var settings = $routeParams.id;
 settings = settings.replace(/[\[\]']+/g,'');
 settings = settings.split(",");
 var parameters = "intensity="+settings[0]+"&focus="+settings[1]+"&format="+settings[2];
 
+// Call service to obatain WOD (workout of the day) by passing configuration parameters.
 $scope.wod = Wod.query({id: parameters});
 
-	$scope.wod.$promise.then(function (result) {
-	    $scope.wod = result;
-	    $scope.reps = $scope.wod[0];
-	 	$scope.exercise = $scope.wod[1];
-		$scope.clock = $scope.wod[2];
+$scope.wod.$promise.then(function (result) {
+    $scope.wod = result;
+    $scope.reps = $scope.wod[0];
+ 	$scope.exercise = $scope.wod[1];
+	$scope.clock = $scope.wod[2];
 
-		$scope.video = Video.query({id: $scope.exercise});
+	// Call Video service pass exercises to use them as hashtags and extract them as videos using Instagram API.
+	$scope.video = Video.query({id: "exercise="+$scope.exercise});
 
-		$scope.video.$promise.then(function (result) {
-			$scope.video = result;
-			$scope.src = $sce.trustAsResourceUrl($scope.video[0]);
-		});		
-	});
+	$scope.video.$promise.then(function (result) {
+		$scope.video = result;
+		$scope.src = $sce.trustAsResourceUrl($scope.video[0]);
+	});		
+});
  
 });
