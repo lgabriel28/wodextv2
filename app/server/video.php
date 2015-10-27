2fe4f65c505c831927f772fad97776b909c059f1
Luis Santiago <luis.santiagoayala@gmail.com>
2015-10-26 21:10:37 -0700

    Clean up video file.

--- app/server/video.php
+++ app/server/video.php
@@ -35,16 +35,6 @@ for($i = 0;$i < $recordCount; $i++){
     }
 }
 
-$video = array(	
-                        array("https://scontent.cdninstagram.com/hphotos-xaf1/t50.2887-16/11771373_976216705734822_1766444921_n.mp4"),
-                        array("https://scontent.cdninstagram.com/hphotos-xaf1/t50.2886-16/11912112_1515344128757030_416273830_n.mp4"),
-                        array("https://scontent.cdninstagram.com/hphotos-xaf1/t50.2886-16/11948803_317833761673966_399287553_n.mp4"),
-                        array("https://scontent.cdninstagram.com/hphotos-xfa1/t50.2886-16/11912048_404602509742035_364470107_n.mp4"),
-                        array("https://scontent.cdninstagram.com/hphotos-xaf1/t50.2886-16/11948889_475886349238779_841600827_n.mp4")
-                );
-
-$num = rand(0, count($video)-1);
-$vid = $video[$num];
 echo json_encode($urlArray);
 
 ?>
